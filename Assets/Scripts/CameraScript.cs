b60fbeb22057d175319c1edd965ac4a92623da1b
levinsonalex <levinsonalex@gmail.com>
2016-02-20 12:09:39 -0500

    First push!

---
+++ Assets/Scripts/CameraScript.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraScript : MonoBehaviour {
+
+    public float lookSpeed = 10f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float mouseX = Input.GetAxis("Mouse X");
+        float mouseY = Input.GetAxis("Mouse Y");
+
+        if(Mathf.Abs(mouseX) > Mathf.Abs(mouseY)) {
+            mouseY = 0;
+        } else
+        {
+            mouseX = 0;
+        }
+
+        transform.Rotate(-mouseY * lookSpeed, mouseX * lookSpeed, 0);
+        transform.rotation = Quaternion.Euler(new Vector3(transform.rotation.eulerAngles.x, transform.rotation.eulerAngles.y, 0));
+	}
+}
---
+++ Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerScript : MonoBehaviour {
+
+    public float speed = 5f;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Rigidbody rb = GetComponent<Rigidbody>();
+        if (rb)
+        {
+            float forwardMovement = Input.GetAxis("Vertical") * speed;
+            float horizontalMovement = Input.GetAxis("Horizontal") * speed;
+
+            Vector3 motion = Camera.main.transform.TransformDirection(new Vector3(horizontalMovement, 0, forwardMovement));
+            rb.velocity = motion;
+        }
+	}
+}
