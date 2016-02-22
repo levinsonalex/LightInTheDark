07d1ee7015c0689d70b31dbd890d69218cfc7715
levinsonalex <levinsonalex@gmail.com>
2016-02-21 20:37:22 -0500

    Made it work for a pedestal!

--- Assets/Scripts/HiddenTextObject.cs
+++ Assets/Scripts/HiddenTextObject.cs
@@ -4,20 +4,28 @@ using System.Collections;
 public class HiddenTextObject : MonoBehaviour {
 
 	Transform tfLight;
+	public GameObject pedestalCamGameObject;
  
 	void Start()
 	{
 		// find the revealing light named "RevealingLight":
-		var goLight = GameObject.Find("FlashlightHead");
-		if (goLight) tfLight = goLight.transform;
+		if (pedestalCamGameObject)
+		{
+			tfLight = pedestalCamGameObject.transform;
+		}
 	}
 
 	void Update()
 	{
-		if (tfLight)
+		if (tfLight && pedestalCamGameObject.activeSelf)
 		{
 			GetComponent<Renderer>().material.SetVector("_LightPos", tfLight.position);
 			GetComponent<Renderer>().material.SetVector("_LightDir", tfLight.forward);
+		}
+		else
+		{
+			GetComponent<Renderer>().material.SetVector("_LightPos", tfLight.position);
+			GetComponent<Renderer>().material.SetVector("_LightDir", Vector3.down);
 		}
 	}
 
