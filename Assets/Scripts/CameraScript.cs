e50bb326c703518cf2be7683a3001f399314fff1
levinsonalex <levinsonalex@gmail.com>
2016-02-21 20:24:50 -0500

    Hidden text done. I am so happy.

--- Assets/Scripts/CameraScript.cs
+++ Assets/Scripts/CameraScript.cs
@@ -3,7 +3,7 @@ using System.Collections;
 
 public class CameraScript : MonoBehaviour {
 
-    public float lookSpeed = 10f;
+    public float lookSpeed = 5f;
 
 	// Use this for initialization
 	void Start () {
