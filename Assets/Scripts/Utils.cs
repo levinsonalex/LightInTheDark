e9c615d05351fcb40ab596f5d0c5a3b70f368465
ConnorUllmann <c.ullmann@hotmail.com>
2016-02-23 02:56:26 -0500

    Worm movement, animation, body parts, and player tracking

---
+++ Assets/Scripts/Utils.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+using System.Collections;
+
+public class Utils : MonoBehaviour {
+    
+    // Use this for initialization
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+
+    public static float angleDiff(float a, float b)
+    {
+        var diff = b - a;
+
+        while (diff > Mathf.PI) { diff -= 2 * Mathf.PI; }
+        while (diff <= -Mathf.PI) { diff += 2 * Mathf.PI; }
+
+        return diff;
+    }
+
+    public static int sign(float x, bool canReturnZero = true)
+    {
+        if (canReturnZero && x == 0)
+            return 0;
+        return x >= 0 ? 1 : -1;
+    }
+
+    public static float easeOutElastic(float t)
+    {
+        var p = 0.3;
+        return Mathf.Pow(2, -10 * t) * Mathf.Sin((float)((t - p / 4f) * (2f * Mathf.PI) / p)) + 1;
+    }
+
+    public static Vector3 mouse { get { return Camera.main.ScreenToWorldPoint(Input.mousePosition); } }
+    public static float GetAngleToMouse(Vector3 pos) { var m = mouse; return Mathf.Atan2(m.y - pos.y, m.x - pos.x); }
+    
+}
