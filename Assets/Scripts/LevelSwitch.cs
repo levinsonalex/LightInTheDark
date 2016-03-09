2e51b7778a76f32934d3a3bb50284fd594eb751b
Robbie Walworth <walworob@umich.edu>
2016-03-08 20:05:19 -0500

    Main room, and both sides room complete | Outfitted with torches and pedestal, just need to add enemies and weapons to the map | Need to add more to level switch scripts so that the player spawns in the right spot when he goes back to the main room

---
+++ Assets/Scripts/LevelSwitch.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class LevelSwitch : MonoBehaviour {
+
+    // Set in Inspector
+    public string levelname;
+
+    void OnTriggerEnter(Collider coll) {
+        SceneManager.LoadScene(levelname);
+    }
+}
