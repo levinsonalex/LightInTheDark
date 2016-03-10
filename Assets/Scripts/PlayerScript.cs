46e7ac34e30bad9db30ba60d09e0e75f40b7ed7b
Robbie Walworth <walworob@umich.edu>
2016-03-10 11:58:52 -0500

    Another bug fix

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -57,12 +57,12 @@ public class PlayerScript : MonoBehaviour {
     // Use this for initialization
     void Start()
     {
-        //// Spawn in the correct spot
-        //if (SpawnPoints.S.prevRoom != 0) {
-        //    transform.position = SpawnPoints.S.spawnPoints[SpawnPoints.S.prevRoom];
-        //    transform.rotation = (SpawnPoints.S.prevRoom == 1) ?
-        //        Quaternion.Euler(0f, 90f, 0f) : Quaternion.Euler(0f, -90f, 0f);
-        //}
+        // Spawn in the correct spot
+        if (SpawnPoints.S.prevRoom != 0) {
+            transform.position = SpawnPoints.S.spawnPoints[SpawnPoints.S.prevRoom];
+            transform.rotation = (SpawnPoints.S.prevRoom == 1) ?
+                Quaternion.Euler(0f, 90f, 0f) : Quaternion.Euler(0f, -90f, 0f);
+        }
 
         normalLocalRotation = sword.transform.localEulerAngles;
 
